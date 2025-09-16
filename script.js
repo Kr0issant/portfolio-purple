9deefc3b228d41958e72ca26328727435abb05ae
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2025-09-16 13:00:39 +0530

    add scroll progress bar

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ document.addEventListener("mousemove", (e) => {
     cursor.style.left = e.clientX + 'px';
     cursor.style.top = e.clientY + 'px';
 })
+
+window.addEventListener("scroll", () => {
+  const progressBar = document.querySelector(".scroll-progress-bar");
+  const documentHeight = document.documentElement.scrollHeight - window.innerHeight;
+  const scrolled = window.scrollY;
+  const scrollPercentage = (scrolled / documentHeight) * 100;
+  progressBar.style.width = scrollPercentage + '%';
+});
