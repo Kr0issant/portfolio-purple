f97dbdd7f0ac240be3c274b9d9230353058d5dd3
Kr0issant <93475036+Kr0issant@users.noreply.github.com>
2025-10-04 12:06:06 +0530

    add glassy circle cursor

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const cursor = document.querySelector(".custom-cursor");
-
 document.addEventListener("mousemove", (e) => {
     cursor.style.left = e.clientX + 'px';
     cursor.style.top = e.clientY + 'px';
 })
 
+const progressBar = document.querySelector(".scroll-progress-bar");
 window.addEventListener("scroll", () => {
-  const progressBar = document.querySelector(".scroll-progress-bar");
   const documentHeight = document.documentElement.scrollHeight - window.innerHeight;
   const scrolled = window.scrollY;
   const scrollPercentage = (scrolled / documentHeight) * 100;
